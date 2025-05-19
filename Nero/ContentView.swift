ce3c44e9942b73465dfdeea94a5a7e142ee7f502
riftpayment <aditya.rai7297@gmail.com>
2025-05-19 11:22:41 -0700

    cleanup

---
+++ Nero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nero
+//
+//  Created by Aditya Rai on 5/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Text("Welcome to Nero")
+                .navigationTitle("Nero")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nero/NeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeroApp.swift
+//  Nero
+//
+//  Created by Aditya Rai on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
